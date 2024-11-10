febf8bc10a1f47a36d9e11ae659745695f1430f7
Daniel-Railsback <daniel.railsback@improvementpath.com>
2024-11-10 09:30:25 -0700

    Test mapping

--- keyboards/keyball/keyball61/keymaps/Dan/keymap.c
+++ keyboards/keyball/keyball61/keymaps/Dan/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BSLS   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_UP     , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_DOWN     , KC_H     , KC_J     , KC_K     , KC_L  , KC_SCLN  ,
     KC_RSFT    , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_LEFT, KC_RGHT     , KC_N     , KC_M  , KC_COMM   , KC_DOT  , KC_QUOTE  ,
-    KC_LCTL  , KC_LALT  , KC_LWIN  , KC_LGUI,LT(1,KC_SPC),LT(2,QK_MOUSE_CURSOR_LEFT),LT(3,QK_MOUSE_CURSOR_RIGHT),    KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_EQL  , KC_PSCR
+    KC_LCTL  , KC_LALT  , KC_LWIN  , KC_LGUI,LT(1,KC_SPC),LT(2,MS_BTN1),LT(3,MS_BTN2),    KC_BSPC,LT(2,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_EQL  , KC_PSCR
   ),
 
   [1] = LAYOUT_universal(
